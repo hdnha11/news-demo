9d6a5f8fa515fe3930b59d7fe7a1078f8aeec5ce
Nha Hoang <hdnha11@gmail.com>
2018-08-06 18:38:49 +0700

    Move API_KEY to config

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
-import { API_HOST } from '../config.json';
+import { API_HOST, API_KEY } from '../config.json';
 
 export const fetchNewsItems = async ({ page } = { page: 1 }) => {
-  const res = await fetch(`${API_HOST}/svc/search/v2/articlesearch.json?api-key=5763846de30d489aa867f0711e2b031c&q=singapore&page=${page}`);
+  const res = await fetch(`${API_HOST}/svc/search/v2/articlesearch.json?api-key=${API_KEY}&q=singapore&page=${page}`);
   const data = await res.json();
 
   return data;
